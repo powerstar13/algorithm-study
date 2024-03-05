1134eb74ad30ffc4875228e91326ff60d92f6190
홍준성 <jshong@sdt.inc>
2024-03-05 22:24:26 +0900

    LeetCode > 1. Two Sum
    - 소스 정리

--- src/main/java/com/example/study/leetcode/array/TwoSum.java
+++ src/main/java/com/example/study/leetcode/array/TwoSum.java
@@ -1,10 +1,13 @@
-package com.example.study.leetcode;
+package com.example.study.leetcode.array;
 
 import org.springframework.stereotype.Component;
 
+/**
+ * https://leetcode.com/problems/two-sum/description/
+ */
 @Component
-public class LeetCodeSolution {
-    public int[] twoSum(int[] nums, int target) {
+public class TwoSum {
+    public int[] solution(int[] nums, int target) {
         int[] result = new int[2];
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
---
+++ src/test/java/com/example/study/leetcode/array/TwoSumTest.java
@@ -0,0 +1,38 @@
+package com.example.study.leetcode.array;
+
+import com.example.study.leetcode.array.TwoSum;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+@SpringBootTest
+class TwoSumTest {
+    @Autowired
+    private TwoSum twoSum;
+
+    @Test
+    void example1() {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] output = {0, 1};
+        assertArrayEquals(output, twoSum.solution(nums, target));
+    }
+
+    @Test
+    void example2() {
+        int[] nums = {3, 2, 4};
+        int target = 6;
+        int[] output = {1, 2};
+        assertArrayEquals(output, twoSum.solution(nums, target));
+    }
+
+    @Test
+    void example3() {
+        int[] nums = {3, 3};
+        int target = 6;
+        int[] output = {0, 1};
+        assertArrayEquals(output, twoSum.solution(nums, target));
+    }
+}
