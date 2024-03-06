2570819294a6008b14512ac391c39de67a7aa620
홍준성 <jshong@sdt.inc>
2024-03-06 22:07:05 +0900

    LeetCode > 9. Palindrome Number
    - Accepted solution
    - Runtime 17ms
    - Memory 44.57MB

--- src/main/java/com/example/study/leetcode/math/PalindromeNumber.java
+++ src/main/java/com/example/study/leetcode/math/PalindromeNumber.java
@@ -2,13 +2,25 @@ package com.example.study.leetcode.math;
 
 import org.springframework.stereotype.Component;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * https://leetcode.com/problems/palindrome-number/description/
  */
 @Component
 public class PalindromeNumber {
     public boolean isPalindrome(int x) {
-        boolean result = false;
-        return result;
+        String numStr = Integer.toString(x);
+        StringBuilder reversedStr = new StringBuilder(numStr).reverse();
+        List<String> leftToRight = new LinkedList<>();
+        List<String> rightToLeft = new LinkedList<>();
+        for (int i = 0; i < numStr.length(); i++) {
+            char leftToRightDigit = numStr.charAt(i);
+            leftToRight.add(Character.toString(leftToRightDigit));
+            char rightToLeftDigit = reversedStr.charAt(i);
+            rightToLeft.add(Character.toString(rightToLeftDigit));
+        }
+        return leftToRight.equals(rightToLeft);
     }
 }
