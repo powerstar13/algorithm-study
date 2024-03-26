7f7ce60792f27c942c53bfa244cbe539199134e3
홍준성 <jshong@sdt.inc>
2024-03-26 22:22:37 +0900

    LeetCode > 13. Roman To Integer
    - Accepted solution
    - Runtime 2ms
    - Memory 44.70MB

---
+++ src/main/java/com/example/study/leetcode/math/RomanToInteger.java
@@ -0,0 +1,40 @@
+package com.example.study.leetcode.math;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * https://leetcode.com/problems/roman-to-integer/description/
+ */
+@Component
+public class RomanToInteger {
+    public int romanToInt(String s) {
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int currentValue = getRomanValue(s.charAt(i));
+            int nextValue = i + 1 < s.length() ? getRomanValue(s.charAt(i + 1)) : 0;
+
+            if (currentValue < nextValue) {
+                result += nextValue - currentValue;
+                i++;
+            } else {
+                result += currentValue;
+            }
+        }
+
+        return result;
+    }
+
+    private int getRomanValue(char c) {
+        return switch (c) {
+            case 'I' -> 1;
+            case 'V' -> 5;
+            case 'X' -> 10;
+            case 'L' -> 50;
+            case 'C' -> 100;
+            case 'D' -> 500;
+            case 'M' -> 1000;
+            default -> throw new IllegalArgumentException("Invalid Roman character: " + c);
+        };
+    }
+}
