c4912def8cc1394d21d0681f7ebd22bc2601f1f3
홍준성 <jshong@sdt.inc>
2024-07-19 22:42:16 +0900

    1. Two Sum
    - for문을 한 번만 사용하도록 리팩토링합니다.
    - Map 데이터타입을 사용하여 key, value로 값을 찾을 수 있도록 합니다.
    - 찾고자하는 target에서 현재 값을 뺀 차이값을 Map에 key로 들어있는지 찾으면 됩니다.

--- src/main/java/com/example/study/leetcode/easy/TwoSum.java
+++ src/main/java/com/example/study/leetcode/easy/TwoSum.java
@@ -1,16 +1,29 @@
 package com.example.study.leetcode.easy;
 
+import java.util.HashMap;
+
 /**
  * https://leetcode.com/problems/two-sum/description/
  */
 class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        int length = nums.length;
-        for (int i = 0; i < length - 1; i++) {
-            for (int j = i + 1; j < length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
+//        int length = nums.length;
+//        for (int i = 0; i < length - 1; i++) {
+//            for (int j = i + 1; j < length; j++) {
+//                if (nums[i] + nums[j] == target) {
+//                    return new int[]{i, j};
+//                }
+//            }
+//        }
+//        return new int[2];
+
+        HashMap<Integer, Integer> numsMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (numsMap.containsKey(diff)) {
+                return new int[]{ i, numsMap.get(diff) };
+            } else {
+                numsMap.put(nums[i], i);
             }
         }
         return new int[2];
