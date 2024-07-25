ec49e24b80e016083679ee79a2ec70cd641a7b6f
홍준성 <jshong@sdt.inc>
2024-07-25 21:21:12 +0900

    13. Roman to Integer
    - 반복되는 if-else if문을 리팩토링합니다.
    - 로마 숫자 표기의 특성은 오른쪽에서 왼쪽으로 숫자가 작아지면 더하고, 오른쪽 문자열보다 왼쪽 문자열이 숫자가 큰 경우 빼야 한다는 것입니다.
    - 이 특성을 사용하여 반복되는 코드를 줄일 수 있습니다.
    - 다만 성능 면에서 기존 코드가 더 빠릅니다.

--- src/main/java/com/example/study/leetcode/easy/RomanToInteger.java
+++ src/main/java/com/example/study/leetcode/easy/RomanToInteger.java
@@ -8,54 +8,73 @@ class RomanToInteger {
     public int romanToInt(String s) {
         int number = 0;
         for (int i = 0; i < s.length(); i++) {
-            char slice = s.charAt(i);
-            char next = ' ';
-            if (i + 1 != s.length()) {
-                next = s.charAt(i + 1);
-            }
-            switch (slice) {
-                case 'I': {
-                    if (next == 'V') {
-                        number += 4;
-                        i++;
-                    } else if (next == 'X') {
-                        number += 9;
-                        i++;
-                    } else {
-                        number += 1;
-                    }
-                    break;
-                }
-                case 'V': number += 5; break;
-                case 'X': {
-                    if (next == 'L') {
-                        number += 40;
-                        i++;
-                    } else if (next == 'C') {
-                        number += 90;
-                        i++;
-                    } else {
-                        number += 10;
-                    }
-                    break;
-                }
-                case 'L': number += 50; break;
-                case 'C': {
-                    if (next == 'D') {
-                        number += 400;
-                        i++;
-                    } else if (next == 'M') {
-                        number += 900;
-                        i++;
-                    } else {
-                        number += 100;
-                    }
-                    break;
-                }
-                case 'D': number += 500; break;
-                case 'M': number += 1000; break;
+//            char slice = s.charAt(i);
+//            char next = ' ';
+//            if (i + 1 != s.length()) {
+//                next = s.charAt(i + 1);
+//            }
+//            switch (slice) {
+//                case 'I': {
+//                    if (next == 'V') {
+//                        number += 4;
+//                        i++;
+//                    } else if (next == 'X') {
+//                        number += 9;
+//                        i++;
+//                    } else {
+//                        number += 1;
+//                    }
+//                    break;
+//                }
+//                case 'V': number += 5; break;
+//                case 'X': {
+//                    if (next == 'L') {
+//                        number += 40;
+//                        i++;
+//                    } else if (next == 'C') {
+//                        number += 90;
+//                        i++;
+//                    } else {
+//                        number += 10;
+//                    }
+//                    break;
+//                }
+//                case 'L': number += 50; break;
+//                case 'C': {
+//                    if (next == 'D') {
+//                        number += 400;
+//                        i++;
+//                    } else if (next == 'M') {
+//                        number += 900;
+//                        i++;
+//                    } else {
+//                        number += 100;
+//                    }
+//                    break;
+//                }
+//                case 'D': number += 500; break;
+//                case 'M': number += 1000; break;
+//            }
+            int current = getValue(s.charAt(i));
+            if (i + 1 < s.length() && current < getValue(s.charAt(i + 1))) {
+                number -= current;
+            } else {
+                number += current;
             }
         }
         return number;
+    }
+
+    private int getValue(char c) {
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
     }
 }
