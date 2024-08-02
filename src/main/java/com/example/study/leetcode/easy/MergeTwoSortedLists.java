a5e9fa87f66bc706578c1bca0509a1091bdcec6e
홍준성 <jshong@sdt.inc>
2024-08-02 18:35:13 +0900

    21. Merge Two Sorted Lists
    - 싱글 링크드 리스트

---
+++ src/main/java/com/example/study/leetcode/easy/MergeTwoSortedLists.java
@@ -0,0 +1,46 @@
+package com.example.study.leetcode.easy;
+
+/**
+ * https://leetcode.com/problems/merge-two-sorted-lists/description/
+ */
+class MergeTwoSortedLists {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+        @Override
+        public String toString() {
+            return "ListNode{" +
+                    "val=" + val +
+                    ", next=" + next +
+                    '}';
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode();
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        if (list1 != null) {
+            current.next = list1;
+        } else {
+            current.next = list2;
+        }
+
+        return dummy.next;
+    }
+}
